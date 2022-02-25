9f3f74d85436843074f50b9b8a6ca66e8ca7002c
Ofek Jacobi <ofek.jacobi@gmail.com>
2022-02-26 01:42:00 +0200

    users table

--- DotNetFramework/pages/AdminHome.aspx
+++ DotNetFramework/pages/AdminHome.aspx
@@ -1,19 +1,22 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="AdminHome.aspx.cs" Inherits="DotNetFramework.pages.AdminHome" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined"
+      rel="stylesheet">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainScreen" runat="server">
     <div>
         <form action="/" method="get">
-
         </form>
     </div>
 
     <div>
-        <table border="1" style="width:100%;">
+        <h2>משתמשי האתר:</h2>
+        <%if (table.Length > 0) {  %>
+        <table border="1" style="width: 100%;">
             <thead>
                 <tr>
-                    <th>שם פרטי</th>
-                    <th>שם משפחה</th>
+                    <th>שם</th>
                     <th>טלפון</th>
                     <th>אימייל</th>
                     <th>סיסמה</th>
@@ -22,22 +25,13 @@
                     <th>תאריך יעד</th>
                     <th>מותג מועדף</th>
                     <th>תיאור</th>
+                    <th>פעולות</th>
                 </tr>
             </thead>
             <tbody>
-                <%
-                    foreach (var user in users)
-                    {
-                        foreach (var field in fields)
-                        {
-                            user[field];
-                        %>
-
-                   <% }} %>
-                <tr>
-                    
-                </tr>
+                <%=table %>
             </tbody>
         </table>
+        <%} %>
     </div>
 </asp:Content>
--- DotNetFramework/pages/AdminHome.aspx.cs
+++ DotNetFramework/pages/AdminHome.aspx.cs
@@ -10,8 +10,8 @@ namespace DotNetFramework.pages
 {
     public partial class AdminHome : System.Web.UI.Page
     {
-        public DataRowCollection users;
-        public string[] fields = { "firstName","lastName","phone", "email","password", "isAdmin","gender","dueDate","favoriteBrand", "dscrptn"};
+        private string[] fields = { "phone", "email", "pswrd", "isAdmin", "gender", "dueDate", "favoriteBrand", "dscrptn" };
+        public string table = "";
 
         private const string dbFileName = "Database.accdb", dbTableName = "table_users";
         protected void Page_Load(object sender, EventArgs e)
@@ -21,8 +21,41 @@ namespace DotNetFramework.pages
                 Response.Redirect("~/pages/Home.aspx");
                 return;
             }
-                
-            users = AdoHelper.ExecuteDataTable(dbFileName, $"SELECT * FROM {dbTableName}").Rows;
+
+            GenerateTable(AdoHelper.ExecuteDataTable(dbFileName, $"SELECT * FROM {dbTableName}").Rows);
+        }
+
+        private void GenerateTable(DataRowCollection users)
+        {
+            string addition;
+            const string updateIcon = "<a class=\"material-icons-outlined btn\">manage_accounts</a>";
+            const string deleteIcon = "<a class=\"material-icons-outlined btn\">person_remove</a>";
+           
+            foreach (DataRow user in users)
+            {
+                addition = (bool)user["isAdmin"] ? "class=\"bold\"" : "";
+
+                table += $"<tr {addition}>\n" +
+                    $"<td>{user["firstName"]} {user["lastName"]}</td>";
+
+                foreach (var field in fields)
+                    table += $"<td>{user[field]}</td>\n";
+
+                table += $"<td class=\"space-around\">{UpdateButton((string)user["email"])}" +
+                    $"{DeleteButton((string)user["email"])}</td>\n" +
+                    $"</tr>\n";
+            }
+        }
+
+        private string UpdateButton(string user)
+        {
+            return $"<a href=\"UpdateUser.aspx?user={user}\" " +
+                $"class=\"material-icons-outlined btn\">manage_accounts</a>";
+        }
+        private string DeleteButton(string user)
+        {
+            return $"<a href=\"DeleteUser.aspx?user={user}\" " +
+                $"class=\"material-icons-outlined btn\">person_remove</a>";
         }
     }
 }
